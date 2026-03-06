00f87a4e0a000ef787b19ec7e8bf8e861b55323e
parikshit <parikshitchauhan12345@gmail.com>
2026-03-07 01:48:42 +0530

    fix cloudinary storage config

--- config/multer.js
+++ config/multer.js
@@ -1,16 +1,18 @@
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('./cloudinary');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-   folder: 'men-drive', 
-    allowed_formats: ['jpg', 'png', 'jpeg', 'pdf', 'mp4'],
-    unique_filename: true, // Ensures unique filenames
+    folder: "men-drive",
+    allowed_formats: ["jpg", "png", "jpeg", "pdf", "mp4"],
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }
+});
 
 module.exports = upload;
